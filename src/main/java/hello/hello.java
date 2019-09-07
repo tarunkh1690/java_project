5d5ee6095e44750e7b68978f6288cb40de3141c1
tarunkh1690 <51506748+tarunkh1690@users.noreply.github.com>
2019-09-07 16:30:12 +0530

    Update hello.java

--- src/main/java/hello/hello.java
+++ src/main/java/hello/hello.java
@@ -1,4 +1,22 @@
-public class main{
-public static void main(String args[]){
-System.out.println("hello World!");
-}}
+
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello Docker World";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
